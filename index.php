b98e63ab4fc9801bc360e55ef5f633f244b928ff
modirock <mmodi.cse@gmail.com>
2017-02-19 12:17:38 +0530

    Included Library in HomePage

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MineWhat assesment</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href="styles/style.css"  rel="stylesheet">
+	<script src="scripts/jquery.min.js"></script>
+	<script src="scripts/script.js"></script>
+</head>
+<body>
+
+</body>
+</html>
