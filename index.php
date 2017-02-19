945d1d12c09613cc6577ca5796064fb849b141a2
modirock <mmodi.cse@gmail.com>
2017-02-19 16:21:14 +0530

    Created Modal Dynamically Using JavaScript

--- index.php
+++ index.php
@@ -9,12 +9,15 @@
 	<script src="scripts/jquery.min.js"></script>
 	<script src="scripts/script.js"></script>
 </head>
-<body>
+<body onload="funFlyIn();">
 	<div class="classHeader" id="idHeaderContent">
 		<h2 id="idHeaderHeading">YOURSTORE.COM</h2>
 	</div>
 	<div class="classMainBody" id="idMainBodyContent">
 		<h2 id="idContentHeading">PAGE CONTENT</h2>
+		<!-- <div class="classMainBodyContent" id="idMainBodyContentModal"> -->
+			
+		</div>
 	</div>
 	<div class="classFooter" id="idFooterContent"></div>
 </body>
