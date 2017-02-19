d3eacbc642cc537bf578deb4b9e88bcdae8f26b3
modirock <mmodi.cse@gmail.com>
2017-02-19 12:51:06 +0530

    Added Heading with stying and OpenSans Font

--- index.php
+++ index.php
@@ -3,11 +3,17 @@
 <head>
 	<title>MineWhat assesment</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="styles/OpenSans.css">
 	<link href="styles/style.css"  rel="stylesheet">
+
 	<script src="scripts/jquery.min.js"></script>
 	<script src="scripts/script.js"></script>
 </head>
 <body>
-
+	<div class="classHeader" id="idHeaderContent">
+		<h2 id="idHeaderHeading">YOURSTORE.COM</h2>
+	</div>
+	<div class="classMainBody" id="idMainBodyContent"></div>
+	<div class="classFooter" id="idFooterContent"></div>
 </body>
 </html>
